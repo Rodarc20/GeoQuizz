235f65c965e0e3230b112fe1cc6c4098b9155dbf
Rodrigo Gutiérrez <rodrigo.gutierrez.k@gmail.com>
2020-05-16 12:45:12 -0500

    Intitial commit

---
+++ app/src/main/java/com/example/geoquizz/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.geoquizz;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
